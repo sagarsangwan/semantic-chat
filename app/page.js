aae9fe4b7325875f8bc805838f1cdb84ab9eb9d4
sagar sangwan <iamsagarsangwan@gmail.com>
2025-05-03 10:35:36 +0530

    removed extra console logs

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import { auth } from "@/lib/auth";
 // import Home from "@/components/Home";
 export default async function Page() {
   const session = await auth();
-  console.log("sessionssssssssssssssssssssssssss", session);
   return (
     <div>{session ? <div> hello {session?.user?.name}</div> : <Welcome />}</div>
   );
