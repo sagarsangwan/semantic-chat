f28db8abaf14cba2c5f492f669599f2c97fd1155
sagar sangwan <iamsagarsangwan@gmail.com>
2025-05-16 16:51:18 +0530

    changes in main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
+import HomeDashboard from "@/components/views/HomeDashboard";
 import Welcome from "@/components/welcome-screen/Welcome";
 import { auth } from "@/lib/auth";
 // import Home from "@/components/Home";
 export default async function Page() {
   const session = await auth();
-  return (
-    <div>{session ? <div> hello {session?.user?.name}</div> : <Welcome />}</div>
-  );
+  return <div>{session ? <HomeDashboard /> : <Welcome />}</div>;
 }
---
+++ components/views/HomeDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppSidebar } from "../layout/app-sidebar";
+
+function HomeDashboard() {
+  return (
+    <div>
+      <AppSidebar />
+    </div>
+  );
+}
+
+export default HomeDashboard;
